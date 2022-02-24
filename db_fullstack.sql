3718cdddfbb0bf3a146b9c75651d9bf93e9633cb
Lucas Romero <52549476+romerolpr@users.noreply.github.com>
2022-02-24 14:28:40 -0300

    Update db_fullstack.sql

--- db_fullstack.sql
+++ db_fullstack.sql
@@ -1,14 +1,61 @@
-CREATE TABLE db_fullstack.tb_users (
-	id INT auto_increment NULL,
-	name varchar(256) NOT NULL,
-	cpf varchar(100) NOT NULL,
-	email varchar(256) NOT NULL,
-	address varchar(100) NOT NULL,
-	last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	description varchar(256) NULL,
-	backdrop varchar(100) DEFAULT #007bff NULL,
-	status BOOL DEFAULT 1 NOT NULL
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;
+-- Host: 127.0.0.1
+-- Tempo de geração: 24/02/2022 às 18:27
+-- Versão do servidor: 10.4.17-MariaDB
+-- Versão do PHP: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db_fullstack`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_users`
+--
+
+CREATE TABLE `tb_users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `cpf` varchar(100) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `description` varchar(256) NOT NULL,
+  `backdrop` varchar(100) NOT NULL DEFAULT '#007bff',
+  `status` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tb_users`
+--
+ALTER TABLE `tb_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_users`
+--
+ALTER TABLE `tb_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
